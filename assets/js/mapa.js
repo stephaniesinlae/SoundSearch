177df3538bf3ef8ba0e3521a0b8841510f832715
stephaniesinlae <stephaniemsa96@gmail.com>
2019-06-20 21:56:12 -0600

    agregar proyecto

---
+++ assets/js/mapa.js
@@ -0,0 +1,294 @@
+// ARRAYS DE EMISORAS DE CADA DEPARTAMENTO
+var eAltaVerapaz = [
+    "- La Pantera 99.1",     //0
+    "- La Sabrosona 103.1",      //1
+    "- Stereo Gerardi 107.9",     //2
+    "- Radio Jesús Mi Fiel Amigo",
+    "- Tezulutlan F.M 103.9",
+  ];
+  
+  var eBajaVerapaz = [
+    "- Stereo Apocalipsis",     //0
+    "- Salama Stereo 91.5",      //1
+  ];
+  
+  var eChimaltenango = [
+    "- Estereo Central 91.9",     //0
+    "- Radio AZ",      //1
+    "- La Chimalteca",     //2
+    "- Radio Carisma 92.7",
+    "- Radio Emisoras Unidas",
+    "- Stereo 100",
+    "- Radio Siglo 104.7",
+  ];
+  
+  var eChiquimula = [
+    "- El poder de Esquipulas 97.5",     //0
+    "- Radio 107 Exito",      //1
+    "- 107.5 La Pegona",     //2
+  ];
+  
+  var ePeten = [
+    "- Radio Peten",     //0
+    "- Estereo Solar",      //1
+    "- Radio mi Redentor",     //2
+    "- Radio Maranatha",
+    "- Stereo 100 ",
+    "- Radio Siglo 104.7",
+    "- La Chimalteca",
+  ];
+  
+  var eElProgreso = [
+    "- Radio Progreso 92.7",     //0
+    "- Radio Libertad Solola 106.5",      //1
+    "- Estereo Amor 87.9, La frecuencia del amor",     //2
+    "- Radio Voz El Carmen",
+    "- Radio la Voz del Príncipe de Paz",
+    "- Radio Progreso Guatemala",
+  ];
+  
+  var eQuiche = [
+    "- La Masheñita",     //0
+    "- Radio La Nueva",      //1
+    "- La Luz del Mundo",     //2
+    "- Swan Tinamit 105.5",
+    "- Chichi 103.1 FM",
+    "- Radio Joya 103.9",
+  ];
+  
+  var eEscuintla = [
+    "- Radio FM Ritmo 106.3",     //0
+    "- Nueva Inspiracion 750",      //1
+    "- Radio Tropicana 94.7",     //2
+    "- La Peligrosa 101.5 y 98.3",
+    "- Pura Candela 99.1",
+    "- Emisoras Unidas",
+  ];
+  
+  var eGuatemala = [
+    "- Radio America Guatemala",     //0
+    "- Radio DJ Guatemala",      //1
+    "- Radio Satdb Guatemala",     //2
+    "- Radio Sin Nombre Internacional de Guatemala",
+    "- Radio Progreso Guatemala",
+    "- Radio Poderosa",
+  ];
+  
+  var eHuehuetenango = [
+    "- Radio Candelero de Barillas Huehuetenango",     //0
+    "- Radio Creativa 98.1",      //1
+    "- Radio Nueva Vida Huehuetenango",     //2
+    "- Radio Jolom Konob",
+    "- Radio Progreso Guatemala",
+    "- Radio La Patrona 97.3",
+  ];
+  
+  var eIzabal = [
+    "- Banana Super Stereo 92.7",     //0
+    "- Caribean 105.1 FM",      //1
+    "- Radio Bahia FM 107.9",     //2
+    "- Shofar FM",
+    "- Radio FeLove",
+    "- Radio Manati",
+  ];
+  
+  var eJalapa = [
+    "- Alcoba Stereo",     //0
+    "- Estereo Adoracion 88.7",      //1
+    "- Galapa Estereo",     //2
+    "- Flow Galapa | Radio Oficial",
+    "- Radio Emisoras Unidas",
+    "- Stereo 100",
+  ];
+  
+  var eJalapa = [
+    "- Alcoba Stereo",     //0
+    "- Estereo Adoracion 88.7",      //1
+    "- Galapa Estereo",     //2
+    "- Flow Galapa | Radio Oficial",
+    "- Radio Emisoras Unidas",
+    "- Stereo 100",
+  ];
+  
+  var eJutiapa = [
+    "- Radio Jutiapa 101 UNO",     //0
+    "- Ke Buena 93.1",      //1
+    "- Radio Emisoras Unidas",     //2
+    "- Stereo 100",
+    "- Radio Siglo 104.7",
+    "- Radio Xtrema 101.3",
+  ];
+  
+  var eQuetzaltenango = [
+    "- Estéreo Visión Xela",     //0
+    "- Diamante 94.3",      //1
+    "- Estereo Ostuncalco",     //2
+    "- Radio TGD",
+    "- La Tesorito 92.7 Puro Ambiente",
+    "- Los Mejores Sones",
+  ];
+  
+  var eReu = [
+    "- Radio Cultural TGN",     //0
+    "- Radio Palabra-Miel",      //1
+    "- La Mejor 103.1 FM",     //2
+    "- La Masheñita",
+    "- Interface Radio",
+    "- Radio La Jefa 101.1 FM",
+  ];
+  
+  var eSacatepequez = [
+    "- Globo Antigua 98.9 FM",     //0
+    "- Antigua FM 91.3",      //1
+    "- Radio Ixchel 102.3 FM",     //2
+    "- Metropolis Antigua 102.9",
+    "- Radio Aliento",
+    "- Radio Amiga 93.3 FM",
+  ];
+  
+  var eSanMarcos = [
+    "- Radio La Voz Celestial",     //0
+    "- Estereo Emanuel",      //1
+    "- Malacatan Estereo",     //2
+    "- Radio San Marcos",
+    "- Radio Stereo Shaddai",
+  ];
+  
+  var eSantaRosa = [
+    "- Radio Camino de Santidad",     //0
+    "- Radio Santa Cruz",      //1
+    "- Radio La Fabulosa 94.1",     //2
+    "- Radio La Nueva",
+  ];
+  
+  var eSolola = [
+    "- Radio Libertad Solola 106.5 FM",     //0
+    "- Radio Emisoras Unidas",      //1
+    "- Radio Siglo 104.7 FM",     //2
+    "- Asi fue mi vida Radio",
+  ];
+  
+  var eSuchitepequez = [
+    "- Radio Indiana 97.9",     //0
+    "- Radio Estrella",      //1
+  ];
+  
+  var eTotonicapan = [
+    "- Radio Comunitaria La Niña 88.5",     //0
+    "- Radio Maranatha 96.5",      //1
+    "- RAdio LA autentica 89.3",
+    "- Estéreo Puacquense",
+    "- Radio Gentsemani",
+    "- Orión Stereo 102.7",
+  ];
+  
+  var eZacapa = [
+    "- Radio Sonora 92.7",
+    "- TGGZ Radio Culturan 93.1",
+    "- Radio Punto 95.5",  
+    "- TGLO Estéreo Azúcar 96.7",
+    "- Ke Buena 98.3",
+    "- La Mega 107.7"
+  ];
+  
+  function CambiarMapa() {
+    // CREANDO VARIABLES PARA UNIR LOS ARRAYS
+    var eChiquimulaJuntos = eChiquimula.join(" \n");
+    var eAltaVerapazJuntos = eAltaVerapaz.join(" \n");
+    var eBajaVerapazJuntos = eBajaVerapaz.join(" \n");
+    var eChimaltenangoJuntos = eChimaltenango.join(" \n");
+    var ePetenJuntos = ePeten.join(" \n");
+    var eElProgresoJuntos = eElProgreso.join(" \n");
+    var eQuicheJuntos = eQuiche.join(" \n");
+    var eEscuintlaJuntos = eEscuintla.join(" \n");
+    var eGuatemalaJuntos = eGuatemala.join(" \n");
+    var eHuehuetenangoJuntos = eHuehuetenango.join(" \n");
+    var eIzabalJuntos = eIzabal.join(" \n");
+    var eJalapaJuntos = eJalapa.join(" \n");
+    var eJutiapaJuntos = eJutiapa.join(" \n");
+    var eQuetzaltenangoJuntos = eQuetzaltenango.join(" \n");
+    var eReuJuntos = eReu.join(" \n");
+    var eSacatepequezJuntos = eSacatepequez.join(" \n");
+    var eSanMarcosJuntos = eSanMarcos.join(" \n");
+    var eSantaRosaJuntos = eSantaRosa.join(" \n");
+    var eSololaJuntos = eSolola.join(" \n");
+    var eSuchitepequezJuntos = eSuchitepequez.join(" \n");
+    var eTotonicapanJuntos = eTotonicapan.join(" \n");
+    var eZacapaJuntos = eZacapa.join(" \n");
+  // JALANDO CON DOM IMAGEN, SECCION, VALUE Y PARRAFO
+    var img = document.getElementById('mapa');
+    var seleccion = document.getElementById('departamentos');
+    var depto = seleccion.options[seleccion.selectedIndex].text;
+    var texto = document.getElementById('demo'); 
+  // CONDICION
+    if(depto === "Alta Verapaz"){
+        texto.innerText = eAltaVerapazJuntos;
+        img.src='../assets/img/mapa/altaverapaz.png';
+    }    else if(depto === "Baja Verapaz"){
+      texto.innerText = eBajaVerapazJuntos;
+      img.src='../assets/img/mapa/bajaverapaz.png';
+    }     else if(depto === "Chimaltenango"){
+      texto.innerText = eChimaltenangoJuntos;
+      img.src='../assets/img/mapa/chimaltenango.png';
+    }else if(depto === "Chiquimula"){
+      texto.innerText = eChiquimulaJuntos;
+      img.src='../assets/img/mapa/chiquimula.png';
+    }else if(depto === "Petén"){
+      texto.innerText = ePetenJuntos;
+      img.src='../assets/img/mapa/peten.png';
+    }else if(depto === "El Progreso"){
+      texto.innerText = eElProgresoJuntos;
+      img.src='../assets/img/mapa/elprogreso.png';
+    }else if(depto === "Quiché"){
+      texto.innerText = eQuicheJuntos;
+      img.src='../assets/img/mapa/quiche.png';
+    }else if(depto === "Escuintla"){
+      texto.innerText = eEscuintlaJuntos;
+      img.src='../assets/img/mapa/escuintla.png';
+    }else if(depto === "Guatemala"){
+      texto.innerText = eGuatemalaJuntos;
+      img.src='../assets/img/mapa/guatemala.png';
+    }else if(depto === "Huehuetenango"){
+      texto.innerText = eHuehuetenangoJuntos;
+      img.src='../assets/img/mapa/huehuetenango.png';
+    }else if(depto === "Izabal"){
+      texto.innerText = eIzabalJuntos;
+      img.src='../assets/img/mapa/izabal.png';
+    }else if(depto === "Jalapa"){
+      texto.innerText = eJalapaJuntos;
+      img.src='../assets/img/mapa/jalapa.png';
+    }else if(depto === "Jutiapa"){
+      texto.innerText = eJutiapaJuntos;
+      img.src='../assets/img/mapa/jutiapa.png';
+    }else if(depto === "Quetzaltenango"){
+      texto.innerText = eQuetzaltenangoJuntos;
+      img.src='../assets/img/mapa/quetzaltenango.png';
+    }else if(depto === "Retalhuleu"){
+      texto.innerText = eReuJuntos;
+      img.src='../assets/img/mapa/reu.png';
+    }else if(depto === "Sacatepéquez"){
+      texto.innerText = eSacatepequezJuntos;
+      img.src='../assets/img/mapa/sacatepequez.png';
+    }else if(depto === "San Marcos"){
+      texto.innerText = eSanMarcosJuntos;
+      img.src='../assets/img/mapa/sanmarcos.png';
+    }else if(depto === "Santa Rosa"){
+      texto.innerText = eSantaRosaJuntos;
+      img.src='../assets/img/mapa/santarosa.png';
+    }else if(depto === "Sololá"){
+      texto.innerText = eSololaJuntos;
+      img.src='../assets/img/mapa/solola.png';
+    }else if(depto === "Suchitepéquez"){
+      texto.innerText = eSuchitepequezJuntos;
+      img.src='../assets/img/mapa/suchitepequez.png';
+    }else if(depto === "Totonicapán"){
+      texto.innerText = eTotonicapanJuntos;
+      img.src='../assets/img/mapa/totonicapan.png';
+    }else if(depto === "Zacapa"){
+      texto.innerText = eZacapaJuntos;
+      img.src='../assets/img/mapa/zacapa.png';
+    }else{
+      texto.innerText = " ";
+      img.src='../assets/img/mapa/mapa.png';
+    }
+  }
